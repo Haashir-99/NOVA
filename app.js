6247111643aa31991e0a9e8b3a6cd2e204145753
Haashir <haashirazhar666@gmail.com>
2024-09-30 09:17:26 +0500

    views folder set

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
